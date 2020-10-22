14c21905892b687ecf68432de405ec872638bfe6
lionking <absoluhs92@gmail.com>
2020-10-22 15:35:02 +0900

    nasdaq_final

--- modules/pattern_labelling.py
+++ modules/pattern_labelling.py
@@ -150,7 +150,7 @@ def labellingD2(d210):
 
 # 나스닥지수 라벨링
 def labellingNASDAQ(d0):
-    
+    i = 0
     nsq_p = d0['close']
     if (nsq_p.iloc[i] - nsq_p.iloc[i+1])/nsq_p.iloc[i] > 0.3:
         if (nsq_p.iloc[i] - nsq_p.iloc[i+1])/nsq_p.iloc[i] < 1.0:
@@ -161,14 +161,15 @@ def labellingNASDAQ(d0):
             return 'U03'
         else : 
             return 'U04'
-    elif (nsq_p.iloc[i] - nsq_p.iloc[i+1])/nsq_p.iloc[i] < -2.8:
-        return 'D04'
-        if (nsq_p.iloc[i] - nsq_p.iloc[i+1])/nsq_p.iloc[i] > -1.7:
-            return 'D03'
+    elif (nsq_p.iloc[i] - nsq_p.iloc[i+1])/nsq_p.iloc[i] > -0.3:
         if (nsq_p.iloc[i] - nsq_p.iloc[i+1])/nsq_p.iloc[i] > -1.0:
-            return 'D02'
-        if (nsq_p.iloc[i] - nsq_p.iloc[i+1])/nsq_p.iloc[i] > -0.3:
             return 'D01'
+        if (nsq_p.iloc[i] - nsq_p.iloc[i+1])/nsq_p.iloc[i] > -1.7:
+            return 'D02'
+        if (nsq_p.iloc[i] - nsq_p.iloc[i+1])/nsq_p.iloc[i] > -2.8:
+            return 'D03'
+        return 'D04'
+                
     else : 
         return 'T00'
 
@@ -188,12 +189,14 @@ def load_stock_with_labels():
     # today에 현재 시간을 불러옵니다.
     today = pd.Timestamp.now()
     today = str(today.year)+str(today.month)+str(today.day)
-    today1 = str(today.year)+'-'+str(today.month)+'-'+str(today.day)
+    # today1 = str(today.year)+'-'+str(today.month)+'-'+str(today.day)
 
     stockData = stock.get_market_ohlcv_by_date("20120101", today, stockCode)
     stockData.columns = pd.Index(
         ["open", "high", "low", "close", "volume"], name=stockData.columns.name)
-    nq = fdr.DataReader('IXIC', '2012-01-01', today1)
+    nq = fdr.DataReader('IXIC', '2012-01-01', today)
+    nq.columns = pd.Index(
+        ["close", "open", "high", "low", "volume", "change"], name=stockData.columns.name)
 
     stockData['pattern1'] = None
     for i in range(len(stockData)):
@@ -214,3 +217,4 @@ def load_stock_with_labels():
     stockData.to_csv(f"{stockData.columns.name}.csv")
     return stockData
 
+load_stock_with_labels()
