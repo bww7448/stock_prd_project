9e291bb6763c2e9e9479827c2ec27b1f088d7383
lionking <absoluhs92@gmail.com>
2020-10-23 13:52:08 +0900

    nasdaq_final

--- modules/pattern_labelling.py
+++ modules/pattern_labelling.py
@@ -148,24 +148,65 @@ def labellingD2(d210):
 
     return res + labellingD0(d210.iloc[2])
 
+# 나스닥지수 한국시간으로 맞추기
+def call_nasdaq():
+    today = pd.Timestamp.now()
+    today = str(today.year)+'-'+str(today.month)+'-'+str(today.day)
+    
+    nq = fdr.DataReader('IXIC', '2012-01-01', today)
+    nq = nq.reset_index()
+    nq.columns = pd.Index(["date", "close", "open","high","low","volume","change"],name=nq.columns.name)
+    
+    # date 쪼개기
+    nq_date = pd.DataFrame(nq['date'])
+    
+    # 아래에 빈 행 추가
+    nq_date.loc['today'] = today
+    nq_date.reset_index(inplace=True)
+    
+    del nq_date['index']
+    
+    # 나머지 컬럼들
+    nq_remain = pd.DataFrame(nq[['close', 'open','change']])
+
+    # 윗 행 생성
+    first_row= {'close':[0],'open':[0],'change':[0]}
+    first_row= pd.DataFrame(first_row)
+
+    # 윗 행 합치기
+    plus_data = pd.concat([first_row, nq_remain])
+
+    #reset_index
+    plus_data.reset_index(inplace=True)
+
+    #del index
+    del plus_data['index']
+    
+    # date + plus_data
+    nsq_data = pd.concat([nq_date, plus_data], axis=1)
+    
+    return nsq_data
+
+nsq_p = call_nasdaq()['change']
+
 # 나스닥지수 라벨링
-def labellingNASDAQ(d0):
-    nsq_p = d0['change']
-    if nsq_p > 0.0003:
-        if nsq_p< 0.0010:
+def labellingNASDAQ(n):
+    # nsq_p = dsq_data['change']
+    if nsq_p[n] > 0.0003:
+        if nsq_p[n]< 0.0010:
             return 'U01'
-        if nsq_p < 0.0017:
+        if nsq_p[n] < 0.0017:
             return 'U02'
-        if nsq_p < 0.0028:
+        if nsq_p[n] < 0.0028:
             return 'U03'
         else : 
             return 'U04'
-    elif nsq_p < -0.0028:
-        if nsq_p > -0.0017:
+    elif nsq_p[n] < -0.0028:
+        if nsq_p[n] > -0.0017:
             return 'D03'
-        if nsq_p > -0.0010:
+        if nsq_p[n] > -0.0010:
             return 'D02'
-        if nsq_p > -0.0003:
+        if nsq_p[n] > -0.0003:
             return 'D01'
         return 'D04'
     else : 
@@ -210,7 +251,7 @@ def load_stock_with_labels():
         
     stockData['nasdaq'] = None
     for i in range(len(stockData)):
-        stockData['nasdaq'].values[i] = labellingNASDAQ(nq.iloc[i])
+        stockData['nasdaq'].values[i] = labellingNASDAQ(i+1)
 
     stockData.to_csv(f"{stockData.columns.name}.csv")
     return stockData
