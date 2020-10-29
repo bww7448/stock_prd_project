15eb24043529931e4513e5316b929d68876526ef
bww7448 <bww7448@gmail.com>
2020-10-29 12:44:00 +0900

    backup

---
+++ modules/item_zzin_nasdaq.py
@@ -0,0 +1,5 @@
+nasdaq_df = pd.read_csv("/resources/nasdaq.csv", parse_dates=['date'], index_col=[0])
+N_item_df = pd.read_csv("/resources/zzin_df_item6.csv", parse_dates=['Date'], index_col=[0])
+
+
+
