a838dbf9a87b10d0de97146cbcada397662c5849
bww7448 <bww7448@gmail.com>
2020-11-20 15:36:18 +0900

    temp save

--- modules/pattern_labelling.py
+++ modules/pattern_labelling.py
@@ -80,6 +80,89 @@ def labellingD0(d0) -> str:
         return "M10"
 
 
+def labellingD0_new(d0, prev_close) -> str:
+    '''
+    D0 시점의 각 봉에 대한 라벨링(25가지)을 수행합니다.
+    당일 1일치(1 row) pd.DataFrame을 입력해주세요.
+    '''
+    openP = d0['open']
+    highP = d0['high']
+    lowP = d0['low']
+    closeP = d0['close']
+    
+    Label1 = 0  # 0,1
+    Label2 = 0  # 0,1
+    Label3 = 0  # 0 ~ 7
+
+
+    # 장대 양봉
+    if closeP >= 1.1*openP:
+        if closeP < highP:  # 윗꼬리 있음
+            if openP > lowP:
+                return "P15"
+            elif (highP - closeP) >= 0.07*prev_close:
+                return "P14"
+            else:
+                return "P13"
+        elif openP > lowP:  # 윗꼬리는 없는데 아랫꼬리는 있음
+            return "P11"
+        else:
+            return "P10"    # 꼬리 없음
+
+    # 짧은 양봉
+    elif closeP >= 1.005*openP:
+        if closeP < highP:
+            if openP > lowP:
+                return "P05"
+            elif (highP - closeP) >= 0.07*openP:
+                return "P04"
+            else:
+                return "P03"
+        elif openP > lowP:
+            if 2*highP - 3*openP + lowP >= 0:
+                return "P01"
+            else:
+                return "P02"
+        else:
+            return "P00"
+
+    # 보합
+    elif closeP >= openP:
+        end_min = closeP - lowP
+        Max_end = highP - closeP
+        if end_min > Max_end*3:
+            return "K01"
+        elif end_min*3 < Max_end:
+            return "K02"
+        return "K00"
+
+    # 짧은 음봉
+    elif closeP >= 0.9*openP:
+        if openP < highP:
+            if closeP > lowP:
+                return "M05"
+            if highP - 3*openP + 2*closeP < 0:
+                return "M03"
+            return "M04"
+        if closeP > lowP:
+            if 3*closeP - lowP - 2*openP < 0:
+                return "M01"
+            return "M02"
+        return "M00"
+
+    # 장대 음봉
+    else:
+        if closeP > lowP:
+            if openP < highP:
+                return "M15"
+            return "M11"
+        if openP < highP:
+            if highP - 2*openP + closeP >= 0:
+                return "M14"
+            return "M13"
+        return "M10"
+
+
 def labellingD1(d10) -> str:
     '''
     D1 5가지 x D0 25가지
